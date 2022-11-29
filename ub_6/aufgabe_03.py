32ec80ada5fc4bab34e96cb60262edb9c98796e2
Serveny <fusion53@web.de>
2022-11-29 19:36:32 +0100

    Kommentar verbessert

--- ub_6/aufgabe_03.py
+++ ub_6/aufgabe_03.py
@@ -1,7 +1,7 @@
 from wypp import *
 
 # Liefert einen Wert zu einem Schluessel aus einer Schluessel-Wert-Paar-Liste zurueck
-# Eingang: Schluessel: str, Schluessel-Wert-Paar-Liste: [(str, Any)]
+# Eingang: Schluessel: str, Schluessel-Wert-Paar-Liste: [(int, Any)]
 # Ausgang: Gefundener Wert zu Schluessel: Union[Any, None]
 def lookup(k: int, l: list[tuple[int, Any]]) -> Union[Any, None]:
     for el in l:
@@ -11,7 +11,7 @@ def lookup(k: int, l: list[tuple[int, Any]]) -> Union[Any, None]:
 
 
 # Sorgt fuer gesetztes Schluessel-Wert-Paar in Liste bei gegebenem Schluessel
-# Eingang: Schluessel-Wert-Paar: (str, Any)
+# Eingang: Schluessel: int, Wert: Any
 # Ausgang: Erweiterte/Veraenderte Liste: list[tuple[str, Any]]
 def insert(k: int, v: Any, l: list[tuple[int, Any]]) -> list[tuple[int, Any]]:
     for i in range(len(l)):
--- vorlesung/polynom.py
+++ vorlesung/polynom.py
@@ -24,5 +24,37 @@ def integrate(p: list[float]) -> list[float]:
     return [0] + [(a / i) for i, a in enumerate(p) if i != 0]
 
 
+def get(l: list[Any], i: int) -> Any:
+    return l[i] if i < len(l) else 0
+
+
+# Polynome addieren
+# Eingang: Zwei Polynome (2 list[float])
+# Ausgang: Addiertes Polynom (list[float])
+# Achtung: Grad der Polynome egal
+def addPoly(p: list[float], q: list[float]) -> list[float]:
+    result = []
+    for i in range(max(len(p), len(q))):
+        ai = get(p, i)
+        bi = get(q, i)
+        result += [ai + bi]
+    return result
+
+
+# Polynome multiplizieren
+def multiPoly(p: list[float], q: list[float]) -> list[float]:
+    if len(p) == 0 or len(q) == 0:
+        return []
+    r = []
+    for i in range(len(p) + len(q) - 1):
+        ci = 0
+        for k in range(0, i + 1):
+            ci += get(p, k) * get(q, i - k)
+        r += [ci]
+    return r
+
+
 check(derivative([]), [])
 check(derivative([42]), [])
+check(addPoly([3, 6, 11], [2, 0, -4, 8]), [5, 6, 7, 8])
+check(multiPoly([1, 2, 3], [1, 1]), [1, 3, 5, 3])
