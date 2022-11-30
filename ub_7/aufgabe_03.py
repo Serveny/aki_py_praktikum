77a593a25b279588db149fa97514527fa66a70c3
Serveny <fusion53@web.de>
2022-11-30 15:26:37 +0100

    Aufgabe 03 Erweiterung

--- ub_7/aufgabe_03.py
+++ ub_7/aufgabe_03.py
@@ -1,17 +1,34 @@
 from wypp import *
 
+# Gibt Zahl mit x und Potenz als String zurueck
+# Eingang: Zahl: float, Exponent von x: int
+# Ausgang: Ausdruck als String
+def zToString(z: float, e: int) -> str:
+    return (
+        str(z)
+        if e == 0
+        else (
+            "x"
+            if z == 1 and e == 1
+            else (f"{z}x" if e == 1 else (f"x^{e}" if z == 1 else f"{z}x^{e}"))
+        )
+    )
+
+
 # Verwandelt Polynom in String-Darstellung
 # Eingang: Polynom: list[float]
 # Ausgang: Polynomstring: str
 def polyToString(p: list[float]) -> str:
-    return " + ".join(
-        [
-            str(z) if i == 0 else (f"{z}x" if i == 1 else f"{z}x^{i}")
-            for i, z in enumerate(p)
-            if z != 0
-        ]
-    )
+    return " + ".join([zToString(z, i) for i, z in enumerate(p) if z != 0])
 
 
 # Tests
+check(zToString(1, 1), "x")
+check(zToString(2, 1), "2x")
+check(zToString(1, 2), "x^2")
+check(zToString(2, 2), "2x^2")
+
+check(polyToString([]), "")
+check(polyToString([0]), "")
+check(polyToString([0, 1, 0, 1]), "x + x^3")
 check(polyToString([8, 5, 0, 0, 9]), "8 + 5x + 9x^4")
