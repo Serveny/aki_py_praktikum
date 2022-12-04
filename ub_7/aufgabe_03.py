772ae4c7475e4a07947632864fb09d2cc4b57941
Serveny <dylan_lewis93@web.de>
2022-12-04 18:15:56 +0100

    Aufgabe 3 neu

--- ub_7/aufgabe_03.py
+++ ub_7/aufgabe_03.py
@@ -1,34 +1,37 @@
 from wypp import *
 
-# Gibt Zahl mit x und Exponent ohne 1 als String zurueck
-# Eingang: Zahl: float, Exponent von x: int
-# Ausgang: Ausdruck als String
-def zToString(z: float, e: int) -> str:
-    return (
-        str(z)
-        if e == 0
-        else (
-            "x"
-            if z == 1 and e == 1
-            else (f"{z}x" if e == 1 else (f"x^{e}" if z == 1 else f"{z}x^{e}"))
-        )
-    )
 
-
-# Verwandelt Polynom in String-Darstellung
-# Eingang: Polynom: list[float]
-# Ausgang: Polynomstring: str
 def polyToString(p: list[float]) -> str:
-    return " + ".join([zToString(z, i) for i, z in enumerate(p) if z != 0])
+    res = ""
+    for i, a in enumerate(p):
+        if a == 0:
+            continue
+
+        # Prefix
+        if res == "":
+            if a < 0:
+                res += "-"
+        else:
+            res += " - " if a < 0 else " + "
+
+        # Number
+        if a != 1:
+            res += str(abs(a))
+
+        # x
+        if i > 0:
+            res += "x"
+
+        # Exponent
+        if i > 1:
+            res += f"^{i}"
+    return res
 
 
 # Tests
-check(zToString(1, 1), "x")
-check(zToString(2, 1), "2x")
-check(zToString(1, 2), "x^2")
-check(zToString(2, 2), "2x^2")
-
 check(polyToString([]), "")
 check(polyToString([0]), "")
 check(polyToString([0, 1, 0, 1]), "x + x^3")
 check(polyToString([8, 5, 0, 0, 9]), "8 + 5x + 9x^4")
+check(polyToString([-8, 5, 0, 0, -9]), "-8 + 5x - 9x^4")
+check(polyToString([-1, 5, 0, 0, -9]), "-1 + 5x - 9x^4")
