c7654653a964e329d887a01b2163d80e45d7e11b
Serveny <dylan_lewis93@web.de>
2022-12-12 18:46:20 +0100

    Aufgabe 3 verbessert

--- ub_8/aufgabe_03.py
+++ ub_8/aufgabe_03.py
@@ -7,13 +7,12 @@ def myInsert(l: list[Any], i: int, x: Any) -> None:
     if i > len(l) - 1:
         l.append(x)
     else:
-        if i < -(len(l) + 1):
+        if i < -len(l):
             i = 0
         l.append(l[-1])
-        r = range(len(l) - 1, len(l) + i if i < 0 else i, -1)
-        for u in r:
+        for u in range(len(l) - 1, (len(l) + i - 1) if i < 0 else i, -1):
             l[u] = l[u - 1]
-        l[i] = x
+        l[(i - 1) if i < 0 else i] = x
 
 
 # Tests
@@ -27,7 +26,7 @@ check(liste_2, [1, 2, 3, "blub", 4])
 
 liste_3 = [1, 2, 3, 4]
 myInsert(liste_3, -2, "blub")
-check(liste_3, [1, 2, 3, "blub", 4])
+check(liste_3, [1, 2, "blub", 3, 4])
 
 liste_4 = [1, 2, 3, 4]
 myInsert(liste_4, 999, "blub")
@@ -35,8 +34,12 @@ check(liste_4, [1, 2, 3, 4, "blub"])
 
 liste_5 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 myInsert(liste_5, -4, "blub")
-check(liste_5, [1, 2, 3, 4, 5, 6, "blub", 7, 8, 9])
+check(liste_5, [1, 2, 3, 4, 5, "blub", 6, 7, 8, 9])
 
 liste_5 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-myInsert(liste_5, -400, "blub")
+myInsert(liste_5, -9, "blub")
 check(liste_5, ["blub", 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+liste_6 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+myInsert(liste_6, -10, "blub")
+check(liste_6, ["blub", 1, 2, 3, 4, 5, 6, 7, 8, 9])
